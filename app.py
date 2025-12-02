e53d61086b9cb05e6e1eef0fee6361b12282e8d2
tarako0isii <kouga.s_0205@icloud.com>
2025-12-02 15:33:56 +0900

    flask動く

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
+import psycopg2
+
 app = Flask(__name__)
+
+
+@app.route('/',methods=['GET'])
+def index():
+      return render_template("index.html")
